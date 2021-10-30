c5ed0ae9b3c154f2f43c5ceefa78bbf5f62aa6f5
Vitaly <ivanov.arduan@yandex.ru>
2021-10-30 09:41:50 +0300

    Маршрутизация запросов.

--- Web_1/urls.py
+++ Web_1/urls.py
@@ -5,5 +5,7 @@ from firstapp import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
     path('admin/', admin.site.urls),
 ]
