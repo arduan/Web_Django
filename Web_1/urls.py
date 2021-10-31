4b803cbf9b8dfed108a7d78691e3a674b604ac82
Vitaly <ivanov.arduan@yandex.ru>
2021-10-31 08:15:39 +0300

    Определение параметров по умолчанию в функции path()

--- Web_1/urls.py
+++ Web_1/urls.py
@@ -4,7 +4,10 @@ from django.urls import path, re_path
 from firstapp import views
 
 urlpatterns = [
+    # Параметры функции path () заключаются в угловые скобки в формате:
+    # <спецификатор:наэвание_параметра>
     path('products/<int:productid>/', views.products, name='products'),
+    path('products/', views.products),
     path('', views.index, name='home'),
     re_path(r'^about/', views.about, name='about'),
     path('contact', views.contact, name='contact'),
--- firstapp/views.py
+++ firstapp/views.py
@@ -15,6 +15,6 @@ def contact(request):
     return HttpResponse('<h3>Контакты</h3>')
 
 
-def products(request, productid):
+def products(request, productid=59):
     output = '<h2>Продукт № {0}</h2>'.format(productid)
     return HttpResponse(output)
