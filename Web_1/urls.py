d1ac32fa6ea9bcc202ba779e199e84651b9ade13
Vitaly <ivanov.arduan@yandex.ru>
2021-10-31 08:03:23 +0300

    Определение параметров через функцию path() более понятно.

--- Web_1/urls.py
+++ Web_1/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, re_path
 from firstapp import views
 
 urlpatterns = [
-    re_path(r'^products/(?P<productid>\d+)/', views.products, name='products'),
+    path('products/<int:productid>/', views.products, name='products'),
     path('', views.index, name='home'),
     re_path(r'^about/', views.about, name='about'),
     path('contact', views.contact, name='contact'),
