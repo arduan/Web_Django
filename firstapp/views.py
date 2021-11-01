f477a002b416b8a5fe975902f8a7d4f18e3070ef
Vitaly <ivanov.arduan@yandex.ru>
2021-11-01 15:36:48 +0300

    Теперь в пути к шаблону страницы показывается и папка, в которой он находится: firstapp/home.html.

--- firstapp/views.py
+++ firstapp/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'firstapp/home.html')
 
 
 def about(request):
