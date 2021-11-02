705fc466b27174fb235e80dce9419627bc9d13aa
Vitaly <ivanov.arduan@yandex.ru>
2021-11-02 13:48:52 +0300

    Использование класса TemplateView.

--- Web_1/urls.py
+++ Web_1/urls.py
@@ -1,13 +1,16 @@
 
 from django.contrib import admin
 from django.urls import path, re_path
+from django.views.generic import TemplateView
+
 from firstapp import views
 
 urlpatterns = [
     path('products/<int:productid>/', views.products, name='products'),
     path('products/', views.products),
     path('', views.index, name='home'),
-    re_path(r'^about/', views.about, name='about'),
+    path('about/', TemplateView.as_view(template_name='firstapp/about.html')),
+    path('contact/', TemplateView.as_view(template_name='firstapp/contact.html')),
     path('contact', views.contact, name='contact'),
     path('admin/', admin.site.urls),
 
