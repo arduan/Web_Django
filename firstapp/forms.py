10a50e5b2d84084811b1f81dcf1a0d651ea2e9a5
Vitaly <ivanov.arduan@yandex.ru>
2021-11-04 08:40:44 +0300

    Изучение разных форм.

--- firstapp/forms.py
+++ firstapp/forms.py
@@ -3,4 +3,6 @@ from django import forms
 class UserForm(forms.Form):
     name = forms.CharField(label='Имя клиента')
     age = forms.IntegerField(label='Возраст клиента')
-    basket =forms.BooleanField(label='Положить товар в корзину')
+    basket =forms.BooleanField(label='Положить товар в корзину', required=False)
+    vyb = forms.NullBooleanField(label='Вы поедите в Сочи этим летом.')
+    email = forms.EmailField.validate(label='Электронный адрес')
